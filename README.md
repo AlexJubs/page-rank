e66587f0a4917ffc45133fc1270df2ca65e220e7
alexjubs <redpod22@gmail.com>
2023-02-12 23:04:30 -0500

    printing pageranks

--- README.md
+++ README.md
@@ -8,7 +8,19 @@ This implementation takes as input an adjacency matrix `G`, representing a graph
 
 The PageRank algorithm uses a mathematical model of the web graph to compute the importance of each page. The web graph is represented as a directed graph, where each node represents a web page and each directed edge represents a link from one page to another. The PageRank of a page is defined as the probability that a random surfer following links on the web will end up on that page after a large number of clicks.
 
-#### Testing:
+#### Updating Values
+
+Firstly all the pagerank values are initialized to 1/N where N is the total number of pages. Thus, all pages have the same initial value.
+
+We update the pagerank value for each page iteratively. Suppose the damping factor is "d", and L(X) is the number out outgoing pages from X. We have a given page "A" with incoming connections from pages B, C, and D. In each iteration, we shall update the pagerank of A as such:
+
+![mathematical formula of the original PageRank](https://cdn1.link-assistant.com/thumbs/w700-c1/upload/news/posts/422/2.png)
+
+#### The Damping Factor
+
+In practice, the PageRank algorithm is modified to handle issues such as the existence of dead ends (pages with no outgoing links) and spider traps (pages with links that point back to themselves). To handle these issues, the algorithm is usually run with a damping factor that reduces the contribution of each link to the PageRank of the target page. The damping factor can be thought of as representing the probability that a random surfer will continue following links, rather than jumping to a random page.
+
+#### Testing
 
 This test case creates a small graph with four nodes and three links, and checks that the PageRank computed by the `page_rank` function is close to the expected result. The `np.allclose` function is used to compare the computed PageRank with the expected result, with a relative tolerance of `1e-6` and an absolute tolerance of `1e-6`. If the computation is correct, the test should pass without raising an exception.
 
--- page_rank.py
+++ page_rank.py
@@ -1,6 +1,6 @@
-import numpy as np
+import copy
 
-def page_rank(G, damping_factor=0.85, epsilon=0.0001):
+def page_rank(G, damping_factor, epsilon):
     """
     Compute the PageRank of nodes in the graph.
     
@@ -18,41 +18,47 @@ def page_rank(G, damping_factor=0.85, epsilon=0.0001):
     numpy array
         The PageRank of each node in the graph.
     """
-    n = G.shape[0]
-    M = np.array(G, dtype=float)
-    for i in range(n):
-        if np.sum(M[i, :]) == 0:
-            M[i,:] = np.ones(n) / n
-        else:
-            M[i,:] /= np.sum(M[i, :])
-            
-    outbound_link_counts = np.sum(G, axis=1)
-    pr = np.ones(n) / n
+    # get the number of pages, n
+    n = len(G)
+
+    # counting how many outbound links there are for each page
+    outbound_link_counts = [sum(row) for row in G]
+    pr = [1.0 / n for _ in range(n)]
     damping_value = (1 - damping_factor) / n
     
+    num_iterations = 0
     while True:
-        new_pr = np.zeros(n)
+        new_pr = [damping_value for _ in range(n)]
         for i in range(n):
             for j in range(n):
-                if outbound_link_counts[j]:
-                    new_pr[i] += damping_factor * M[i, j] * pr[j] / outbound_link_counts[j]
-                else:
-                    new_pr[i] += damping_factor * M[i, j] * pr[j]
-                    
-        new_pr += damping_value
-        delta = np.abs(pr - new_pr).sum()
+                # for each incoming node onto i
+                if G[j][i] != 1: continue
+                new_pr[i] += (damping_factor * pr[j]) / outbound_link_counts[j]
+        
+        # new_pr = [value + damping_value for value in new_pr]
+        delta = sum(abs(pr[i] - new_pr[i]) for i in range(n))
         if delta <= epsilon:
+            print(f"terminated after {num_iterations} iterations")
             return new_pr
-        pr = new_pr
+        print(f"pagerank after {num_iterations} iterations:")
+        print(pr)
+        pr = copy.deepcopy(new_pr)
+        num_iterations += 1
+
+def pretty_print(M):
+    for row in M: print(row)
 
 def test_page_rank():
-    G = np.array([[0, 1, 1, 0],
-                  [1, 0, 0, 0],
-                  [1, 0, 0, 1],
-                  [0, 0, 1, 0]])
-    true_pr = np.array([0.2195122, 0.36585366, 0.36585366, 0.14634146])
+    # 1 on (i,j) indicates an outbound link from i to j
+    G = [[0, 1, 1, 0],
+         [1, 0, 0, 0],
+         [1, 0, 0, 1],
+         [0, 0, 1, 0]]
+    # 0 -> (1,2), 1 -> (0), 2 -> (0,3), 3 -> (2)
+    print(f"testing pagerank with {len(G)} pages, with the following initial popularity scores:")
     pr = page_rank(G, damping_factor=0.85, epsilon=0.0001)
-    assert np.allclose(pr, true_pr, rtol=1e-6, atol=1e-6)
+    print("Final PageRank scores (for pages 1-4):")
+    print(pr)
 
 if __name__ == "__main__":
     test_page_rank()
